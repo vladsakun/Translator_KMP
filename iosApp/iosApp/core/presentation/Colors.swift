842c4604fb7dc73f0f27e271ed51444e46c8d443
vlad <vlad120403@gmail.com>
2024-04-21 18:09:22 +0200

    Add ios colors

---
+++ iosApp/iosApp/core/presentation/Colors.swift
@@ -0,0 +1,62 @@
+//
+//  Colors.swift
+//  iosApp
+//
+//  Created by Vladyslav Sakun on 21.04.2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import shared
+
+// What?)
+extension Color {
+    // What
+    init(hex: Int64, alpha: Double = 1){
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+    
+    private static let colors = Colors()
+    static let lightBlue = Color(hex: colors.LightBlue)
+    static let lightBlueGrey = Color(hex: colors.LightBlueGrey)
+    static let accentViolet = Color(hex: colors.AccentViolet)
+    static let textBlack = Color(hex: colors.TextBlack)
+    static let darkGrey = Color(hex: colors.DarkGrey)
+    
+    static let primary = Color(light: .accentViolet, dark: .accentViolet)
+    static let background = Color(light: .lightBlueGrey, dark: .darkGrey)
+    static let onPrimary = Color(light: .white, dark: .white)
+    static let onBackground = Color(light: .textBlack, dark: .white)
+    static let surface = Color(light: .white, dark: .darkGrey)
+    static let onSurface = Color(light: .textBlack, dark: .white)
+}
+
+// SwiftUI color
+private extension Color {
+    init(light: Self, dark: Self){
+        self.init(uiColor: UIColor(light: UIColor(light), dark: UIColor(dark)))
+    }
+}
+
+// UIKit color
+private extension UIColor{
+    convenience init(light: UIColor, dark: UIColor){
+        self.init { traits in
+            switch traits.userInterfaceStyle {
+            case .light, .unspecified:
+                return light
+            case .dark:
+                return dark
+            @unknown default:
+                return light
+            }
+        }
+    }
+}
